3707d021cd3ebe9af19260fc1c6b46cb45926648
kimjooyoung <158056472+kju0@users.noreply.github.com>
2024-03-21 19:53:19 +0900

    [Silver IV] Title: 큐 2, Time: 2492 ms, Memory: 174820 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,36 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 N = int(input())
 
-deq = deque()
+q = []
+first = 0
 
 for _ in range(N):
     input_str = list(map(str, input().split()))
     
     if input_str[0] == "push":
-        deq.append(input_str[1])
+        q.append(input_str[1])
     elif input_str[0] == "pop":
-        if len(deq) == 0:
-            print(-1)
+        if len(q) > first:
+            print(q[first])
+            first += 1
         else:
-            print(deq.popleft())
-    if input_str[0] == "size":
-        print(len(deq))
-    if input_str[0] == "empty":
-        if len(deq) == 0:
+            print(-1)
+    elif input_str[0] == "size":
+        print(len(q)-first)
+    elif input_str[0] == "empty":
+        if len(q) == first:
             print(1)
+            q = []
+            first = 0
         else:
             print(0)
-    if input_str[0] == "front":
-        if len(deq) == 0:
+    elif input_str[0] == "front":
+        if len(q) <= first:
             print(-1)
         else:
-            print(deq[0])
-    if input_str[0] == "back":
-        if len(deq) == 0:
+            print(q[first])
+    elif input_str[0] == "back":
+        if len(q) <= first:
             print(-1)
         else:
-            print(deq[-1])
+            print(q[-1])
