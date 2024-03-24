c3baef09b921bd68781e9e1ab749de84beda4367
kimjooyoung <158056472+kju0@users.noreply.github.com>
2024-03-24 13:54:26 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1896 ms, Memory: 99592 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -8,38 +8,31 @@ roads = [[] for _ in range(N+1)]
 
 for _ in range(M):
     a, b = map(int,input().split())
+
     roads[a].append(b)
 
 #print(roads)
 
-dist = [0]*(N+1)
-visited = [False]*(N+1)
-answer = []
+answer = [int(1e9)]*(N+1)
 #X에서 출발했을 때 최단거리가 K인 모든 도시를 구해라!
 #하나도 없는 경우 -1
 
 dq = deque()
 dq.append(X)
 
-visited[X] = True
-dist[X] = 0
+answer[X] = 0
+
 while dq:
     idx = dq.popleft()
 
     for i in roads[idx]:
-        if not visited[i]:
-            visited[i] = True
+        if answer[i] > answer[idx]+1:
+            answer[i] = answer[idx]+1
             dq.append(i)
-            dist[i] = dist[idx] + 1
-            if dist[i] == K:
-                answer.append(i)
 
-
-if len(answer) == 0:
+if answer.count(K) == 0:
     print(-1)
 else:
-    answer.sort()
-    for i in answer:
-        print(i)
-
-
+    for i in range(1, N+1):
+        if answer[i] == K:
+            print(i)
